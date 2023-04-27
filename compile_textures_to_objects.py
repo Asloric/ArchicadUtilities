249445f89eb2d2d10f1f913d01399c2b5e0f24c7
Asloric <43351133+Asloric@users.noreply.github.com>
2023-04-28 00:08:49 +0200

    Fixed Cdata missing

--- compile_textures_to_objects.py
+++ compile_textures_to_objects.py
@@ -1,19 +1,49 @@
 import xml.etree.ElementTree as ET
+import xml.sax.saxutils as su
 import os
-import shutil
 
-xml_folder = 'C:\\Users\\clovis.flayols\\Desktop\\Object surface converter\\surfaces XML'
-xml_attribute_folder = 'C:\\Users\\clovis.flayols\\Desktop\\Object surface converter\\surfaces XML attribute'
-gsm_texture_folder = 'C:\\Users\\clovis.flayols\\Desktop\\Object surface converter\\surfaces GSM\\[TImg] Textures'
+xml_folder = 'E:\\Work\\Pro\\Christian FLOUR\\Bibliotheque Archicad\\Object surface converter\\surfaces XML'
+xml_attribute_folder = 'E:\\Work\\Pro\\Christian FLOUR\\Bibliotheque Archicad\\Object surface converter\\surfaces XML attribute'
+gsm_texture_folder = 'E:\\Work\\Pro\\Christian FLOUR\\Bibliotheque Archicad\\Object surface converter\\surfaces GSM\\[TImg] Textures'
 cached_texture_folder = {}
-objet_xml_folder = 'C:\\Users\\clovis.flayols\\Desktop\\Object surface converter\\objets XML'
-
+objet_xml_folder = 'E:\\Work\\Pro\\Christian FLOUR\\Bibliotheque Archicad\\Object surface converter\\objets XML'
+output_folder = 'C:\\Users\\Asloric\\Desktop\\'
+warning_level = 3
 
 #============================================================================================================================================
 #============================================================================================================================================
 #============================================================================================================================================
 
-class SURFACES():
+def CDATA(text=None):
+    element = ET.Element('![CDATA[')
+    element.text = text
+    return element
+
+
+
+ET._original_serialize_xml = ET._serialize_xml
+
+def _serialize_xml(write, elem, qnames, namespaces,short_empty_elements, **kwargs):
+
+    if elem.tag == '![CDATA[':
+        write("\n<{}{}]]>\n".format(elem.tag, elem.text))
+        if elem.tail:
+            write(_escape_cdata(elem.tail))
+    else:
+        return ET._original_serialize_xml(write, elem, qnames, namespaces,short_empty_elements, **kwargs)
+
+ET._serialize_xml = ET._serialize['xml'] = _serialize_xml
+
+class METACLASS():
+
+    def debug(self, text, level=0):
+        global warning_level
+        info_level = ["<INFO>", "<WARNING>", "<ERROR>"]
+        if level >= warning_level:
+            infos = ''.join(str(t) for t in text)
+            print(info_level[level], infos)
+
+class SURFACES(METACLASS):
     '''Informations de représentation des surfaces dans les scripts. Script_1D compatible avec objets.'''
     instances = {}
 
@@ -39,7 +69,7 @@ class SURFACES():
                 
                 
     def print(self):
-        print(surface.name)
+        print(self.name)
         for attr in dir(self):
             if not callable(getattr(self, attr)) and not "__" in attr:
                 print("    " + attr) #, type(getattr(self, attr)))
@@ -73,12 +103,8 @@ class SURFACES():
         script = []
         lines = self.Script_1D.text.split("\n")
         for line in lines:
-            if line.startswith("<![CDATA["):
-                script.append(line.replace("<![CDATA[", ""))
-            elif line.startswith("call "):
+            if line.startswith("call "):
                 pass
-            elif line.endswith("]]>"):
-                script.append(line.replace("]]>", ""))
             else:
                 if new_material_name:
                     if line.startswith("define material "):
@@ -97,7 +123,7 @@ class SURFACES():
 #============================================================================================================================================
 #============================================================================================================================================
 
-class SURFACES_ATTRIBUTE():
+class SURFACES_ATTRIBUTE(METACLASS):
     instances = {}
     
     def __init__(self, index, name):
@@ -129,13 +155,13 @@ class SURFACES_ATTRIBUTE():
             self.surface = surface_instance
             
         else:
-            print("<WARNING> unassociated material:", self.index, self.name, "because the '/' is not allowed. Please avoid it in names.")
+            super().debug(("unassociated material:", self.index, self.name, "because the '/' is not allowed. Please avoid it in names."), 0)
 
 #============================================================================================================================================
 #============================================================================================================================================
 #============================================================================================================================================
 
-class OBJET(): 
+class OBJET(METACLASS): 
     unknown_materials = [] # Les matériaux non trouvés sont mis ici
     instances = {}
 
@@ -148,12 +174,12 @@ class OBJET():
         self.texture_folder = None
         self.preview = None
         self.sections_list = []
+        self.cdata_sections_list = []
         OBJET.instances[mainguid] = self
             
 
     @classmethod
     def load_files(cls, path, original_path):
-        global debug_i
         for file in os.scandir(path):
             if file.is_dir():
                 OBJET.load_files(file.path, original_path)
@@ -172,18 +198,24 @@ class OBJET():
                 for section in xml_data:
                     # Si il ya déjà une section de ce nom, créé une liste à la place.
                     if hasattr(objet_instance, section.tag):
-                        if type(getattr(object_instance, section.tag)) is not list:
-                            setattr(objet_instance, section.tag, [getattr(object_instance, section.tag)])
-                            object_instance.section_list.append(section.tag)
+                        if type(getattr(objet_instance, section.tag)) is not list:
+                            setattr(objet_instance, section.tag, [getattr(objet_instance, section.tag)])
+                            
                             
                         exec(f"objet_instance.{section.tag}.append(section)")
                             
                     else:
                         setattr(objet_instance, section.tag, section)
-                
+
+
+                    if section.text not in ["\n\t", None]:
+                        if section.text == "\n\n":
+                            section.text = ""
+                        objet_instance.cdata_sections_list.append(section.tag)
+                    objet_instance.sections_list.append(section.tag)
+
                 objet_instance.find_textures()
 
-            
     def print(self):
         print(self.name)
         for attr in dir(self):
@@ -217,7 +249,6 @@ class OBJET():
             self.textures[GDLPict.get("SubIndent")] = xml_folder + GDLPict.get("path").replace("/", os.sep)
             self.texture_folder = os.path.dirname(xml_folder + GDLPict.get("path").replace("/", os.sep))
 
-        
     def merge_materials(self):
         '''Trouve toutes les surfaces référencées dans les paramètres de l'objet, et les répertorie dans les attributs de la classe.
         Dans un second temps, cherche la surface correspondante dans la class SURFACE pour demander intégration.'''
@@ -240,12 +271,12 @@ class OBJET():
                     for texture_name, texture in surface_instance.textures.items():
                         self.add_gdlpict(texture)
                 else:
-                    print(f"<WARNING> Surface introuvable pour le surface_attribute {surface_attribute.name}")
+                    super().debug((f"Surface introuvable pour le surface_attribute {surface_attribute.name}"), 1)
 
             else:
                 if not material_index in OBJET.unknown_materials:
                     OBJET.unknown_materials.append(material_index)
-                print(f"<WARNING> Matériau {material_name} avec index {material_index} non trouvé dans l'objet {self.name}")
+                super().debug((f"Matériau {material_name} avec index {material_index} non trouvé dans l'objet {self.name}"), 2)
                 
         
         # récupère le Script_1D de l'objet
@@ -256,20 +287,15 @@ class OBJET():
             script_1d_materials += "FRAGMENT2 ALL, 0"
             
         # Recompile les scripts en un string.
-        self.set_script_1d(script_1d_objet + script_1d_materials)
-        
-        
-        
-        
-        
-            
+        self.set_script_1d(script_1d_objet + script_1d_materials)    
+
     def get_script_1d(self):
         if not hasattr(self, "Script_1D"):
             data = '''<Script_1D SectVersion="20" SectionFlags="0" SubIdent="0">
 <![CDATA[]]>
 </Script_1D>'''  
             self.Script_1D = ET.fromstring(data)
-            print(f"Script_1D manquant dans objet {self.name}")
+            super().debug((f"Script_1D manquant dans objet {self.name}"), 0)
         script = []
         lines = self.Script_1D.text.split("\n")
         for line in lines:
@@ -288,33 +314,60 @@ class OBJET():
             script_str += line + "\n"
         script_str += "]]>\n"
 
-
     def add_gdlpict(self, texture):
         ext = texture.name.split(".")[-1]
         if not hasattr(self, "GDLPict"):
             setattr(self, "GDLPict", [])
         data = f'''<GDLPict MIME="image/{ext}" SectVersion="19" SectionFlags="0" SubIdent="{len(self.GDLPict)}" path="{texture.path}"/>'''
         self.GDLPict.append(ET.fromstring(data))
-        
+
     def compile_xml(self):
-        attributes = [attr for attr in dir(self) if attr in self.sections_list + ["GDLPict", "Script_1D"]]
+        attributes = [attr for attr in dir(self) if attr in self.sections_list + ["GDLPict", "Script_1D"]]                
+                
+        # Créer un nouveau xml
+        tree = ET.ElementTree()
+
+        # Créer un nouvel élément racine pour le xml
+        root = ET.Element("Symbol")
+        for attrib, value in self.xml_data.attrib.items():
+            root.attrib[attrib] = value
+
+        # Ajouter les sections au xml
         for attr in attributes:
-            print(str(type(getattr(self, attr))), attr)
-            if "xml.etree.ElementTree.Element" in str(type(getattr(self, attr))):
-                self.xml_data.set(attr, getattr(self, attr))
-            elif type(getattr(self, attr)) is list:
-                for element in getattr(self, attr):
-                    self.xml_data.set(element.tag, element)
-        
-        output_path = 'C:\\Users\\clovis.flayols\\Desktop\\Object surface converter\\objects XML converted\\' + self.relative_path + "\\"
+            # Si la section est une liste, ajouter chaque élément de la liste à l'arbre
+            if isinstance(getattr(self, attr), list):
+                for item in getattr(self, attr):
+                    if item.tag in self.cdata_sections_list:
+                        Ctext = CDATA(item.text)
+                        item.text = None
+                        item.append(Ctext)
+                        root.append(item)
+                    else:
+                        root.append(item)
+            # Sinon, ajouter directement la section à l'arbre
+            else:
+                section = getattr(self, attr)
+                if section.tag in self.cdata_sections_list:
+                    Ctext = CDATA(section.text)
+                    section.text = None
+                    section.append(Ctext)
+                    root.append(section)
+                else:
+                    root.append(section)
+
+        # Ajouter l'élément racine à l'arbre
+        tree._setroot(root)
+
+        global output_folder
+        output_path = output_folder + self.relative_path + "\\"
         if not os.path.exists(output_path):
             os.makedirs(output_path)
         file_path = output_path + self.name + ".xml"
-        new_tree = ET.ElementTree(self.xml_data)
-        new_tree.write(file_path, encoding="utf-8", xml_declaration=True)
-                
-                
-                
+
+        # Enregistrer l'arbre dans un fichier XML
+        tree.write(file_path, encoding="utf-8", xml_declaration=True)
+
+
 # ================================================================================================================
 # ================================================================================================================
 # ================================================================================================================
@@ -335,8 +388,10 @@ OBJET.load_files(objet_xml_folder, objet_xml_folder)
 # Combine les surfaces dans l'objet.
 for objet in OBJET.instances.values():
     objet.merge_materials()
-    objet.compile_xml()
-    
+
+objet = list(OBJET.instances.values())[0]
+objet.compile_xml()
+
 
 # cleanup
 SURFACES.instances = {}
